6959d21eceb2c646d9377b68fb4419a741472bb2
mrpumpkin98 <sju4486@naver.com>
2023-08-25 23:36:53 +0900

    feat : 폰트추가

--- src/commons/styles/globalStyles.ts
+++ src/commons/styles/globalStyles.ts
@@ -4,5 +4,46 @@ export const globalStyles = css`
   * {
     margin: 0px;
     box-sizing: border-box;
+    font-family: "Arita-Medium", "NotoSansKR-Regular";
+  }
+  @font-face {
+    font-family: "myfont";
+    src: url("/fonts/scifibit.ttf");
+  }
+
+  // bold
+  @font-face {
+    font-family: "Arita-Bold";
+    src: url("/fonts/arita-dotum-Bold.woff");
+  }
+
+  // semi-bold
+  @font-face {
+    font-family: "Arita-SemiBold";
+    src: url("/fonts/arita-dotum-SemiBold.woff");
+  }
+
+  // medium
+  @font-face {
+    font-family: "Arita-Medium";
+    src: url("/fonts/arita-dotum-Medium.woff");
+  }
+
+  // light
+  @font-face {
+    font-family: "Arita-Light";
+    src: url("/fonts/arita-dotum-Light.woff");
+  }
+
+  // thin
+  @font-face {
+    font-family: "Arita-Thin";
+    src: url("/fonts/arita-dotum-Thin.woff");
+  }
+
+  // alt font
+  @font-face {
+    font-family: "NotoSansKR-Regular";
+    src: url("/fonts/NotoSansKR-Regular.woff");
   }
 `;
