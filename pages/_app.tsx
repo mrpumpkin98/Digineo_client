64c25beb8017c41d8fe143451a400b0cdd3e509f
mrpumpkin98 <sju4486@naver.com>
2023-08-26 09:55:14 +0900

    feat : 로고, 타이틀 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,16 @@ import { AppProps } from "next/app";
 import { Global } from "@emotion/react";
 import { globalStyles } from "../src/commons/styles/globalStyles";
 import { RecoilRoot } from "recoil";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <RecoilRoot>
+        <Head>
+          <title>디기너 - 디깅모멘텀 유형검사 서비스</title>
+          <link rel="icon" type="image/png" href="/images/logo2.png" />
+        </Head>
         <Global styles={globalStyles} />
         <Component {...pageProps} />
       </RecoilRoot>
